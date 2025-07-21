aacd7744c41be8cfeaf379cd77d742c851190b61
Chun Chung Cheng <139341900+JCheng0709@users.noreply.github.com>
2025-07-21 20:25:47 +0800

    Delete mkdir.py

--- mkdir.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-root = Path(r"C:\Users\ivers\OneDrive\桌面\stock-project")
-root.mkdir(parents=True, exist_ok=True)
-
-for year in range(2016, 2026):                      # 2016 ~ 2025
-    (root / str(year)).mkdir(exist_ok=True)
-    print("✓ 建立 / 已存在：", root / str(year))
